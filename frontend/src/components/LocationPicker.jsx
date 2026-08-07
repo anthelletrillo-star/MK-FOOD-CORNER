4f6b52b69b48998922584093ba328559580824df
asdasdasd1213267 <github1437@gmail.com>
2026-08-07 12:50:30 +0800

    5

--- frontend/src/components/LocationPicker.jsx
+++ frontend/src/components/LocationPicker.jsx
@@ -225,8 +225,8 @@ export default function LocationPicker({ onLocationSelect, initialAddress = '' }
           scrollWheelZoom={false}
         >
           <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://cartodb.com/attributions">CartoDB</a>'
+            url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png"
           />
           <LocationMarker position={position} setPosition={setPosition} setAddress={setAddress} />
           {position && <ChangeView center={position} />}
