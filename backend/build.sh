b0e5e66e2a1c7967f7691ee7160d7bdbdd1845b1
asdasdasd1213267 <github1437@gmail.com>
2026-08-31 17:49:49 +0800

    ambert

--- backend/build.sh
+++ backend/build.sh
@@ -3,5 +3,5 @@
 npm install
 # Generate Prisma Client
 npx prisma generate
-# Optional: Sync database (Only run this manually or if you're sure)
-# npx prisma db push --accept-data-loss
+# Sync database schema with Prisma schema
+npx prisma db push
